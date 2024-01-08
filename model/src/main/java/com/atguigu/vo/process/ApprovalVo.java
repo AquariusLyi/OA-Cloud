5b2c3bb8c0b7019a39cfb4549a8997c84956776c
liyi <18684997125@163.com>
2024-01-08 21:17:13 +0800

    解决：easyexcel 科学计数法与千分位 原因：

--- model/src/main/java/com/atguigu/vo/process/ApprovalVo.java
+++ model/src/main/java/com/atguigu/vo/process/ApprovalVo.java
@@ -1,13 +1,18 @@
 package com.atguigu.vo.process;
 
+import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.converters.bigdecimal.BigDecimalNumberConverter;
+import com.alibaba.excel.converters.bigdecimal.BigDecimalStringConverter;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class ApprovalVo {
 
+    //        @ExcelProperty(value = "Number", index = 0, converter = BigDecimalConverter.class)
+    @ExcelProperty(value = "Number", index = 0, converter = BigDecimalNumberConverter.class)
     private Long processId;
-
+    @ExcelProperty(value = "Number", index = 0, converter = BigDecimalStringConverter.class)
     private String taskId;
 
     @ApiModelProperty(value = "状态")
